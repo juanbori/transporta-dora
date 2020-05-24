5ef97fe1ad8fe8fb07f2d543c4fb391ce23f936d
juanbori <jsires94@hotmail.com>
2020-05-24 13:37:13 -0300

    commit inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Dora-Transportadora"/>
+    <HelloWorld msg="Dora-Transportadora."/>
   </div>
 </template>
 

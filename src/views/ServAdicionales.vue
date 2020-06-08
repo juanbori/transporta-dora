392b979cf8d68803cecd2eddf34a7089a2cb4941
Alvaro <gonmaralv@gmail.com>
2020-06-08 17:02:43 -0300

    Agregado de vista Vehiculos con cards

---
+++ src/views/ServAdicionales.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="Formulario">
+    <h1>Esto es Servicios Adiconales</h1>
+    <router-view></router-view>
+  </div>
+</template>
--- src/views/Vehiculos.vue
+++ src/views/Vehiculos.vue
@@ -1,5 +1,64 @@
 <template>
-  <div class="vehiculos">
-    <h1>This is an vehiculos page</h1>
+  <div class="card-deck">
+    <div class="card">
+      <img src="../assets/Vehiculos/Moto.jpg" class="card-img-top"> 
+            <div class="card-body">     
+              <h5 class="card-title">MOTO</h5>
+              <p class="card-text">$100 x día</p>
+              <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
+              <button @click="irContratar" class="btn btn-primary">Contratar</button><br>
+            </div>
+    </div>
+
+    <div class="card">
+      <img src="../assets/Vehiculos/Kangoo.jpg" class="card-img-top"> 
+            <div class="card-body">     
+              <h5 class="card-title">KANGOO</h5>
+              <p class="card-text">$150 x día</p>
+             <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
+             <button @click="irContratar" class="btn btn-primary">Contratar</button><br>
+            </div>
+    </div>
+  
+    <div class="card">
+      <img src="../assets/Vehiculos/F100.jpg" class="card-img-top"> 
+            <div class="card-body">     
+              <h5 class="card-title">F100</h5>
+              <p class="card-text">$200 x día</p>
+              <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
+              <button @click="irContratar" class="btn btn-primary">Contratar</button><br>
+            </div>
+    </div>
+    
+    <div class="card">
+      <img src="../assets/Vehiculos/Camion.jpg" class="card-img-top"> 
+            <div class="card-body">     
+              <h5 class="card-title">CAMIÓN</h5>
+              <p class="card-text">$250 x día</p>
+              <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
+              <button @click="irContratar" class="btn btn-primary">Contratar</button><br>
+            </div>
+    </div>
+    
+          
   </div>
 </template>
+
+
+
+
+
+
+<script>
+export default {
+  name: 'Vehiculos',
+  methods:{
+    irDescripcion(){
+      this.$router.push('/ServAdicionales')
+    },
+    irContratar(){
+      this.$router.push('/Formulario')
+    }
+  }
+}
+</script>

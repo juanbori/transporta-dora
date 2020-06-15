630392b139f967e6e7bd5332416aaeb4094c7f89
Alvaro <gonmaralv@gmail.com>
2020-06-15 15:39:06 -0300

    Agregado de servicios adicionales

--- src/views/Vehiculos.vue
+++ src/views/Vehiculos.vue
@@ -1,64 +1,65 @@
 <template>
-  <div class="card-deck">
-    <div class="card mt-5">
-      <img src="../assets/Vehiculos/Moto.jpg" class="card-img-top"> 
-            <div class="card-body">     
-              <h5 class="card-title">MOTO</h5>
-              <p class="card-text">$100 x día</p>
-              <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
-              <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button><br>
-            </div>
+  <div class="card-deck mx-auto">
+    <div class="card mt-5 p-3">
+      <img src="../assets/Vehiculos/Moto.png" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">MOTO</h5>
+        <p class="card-text">$100 x día</p>
+        <button @click="irDescripcion" class="btn btn-primary">Descripción</button>
+        <br />
+        <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button>
+        <br />
+      </div>
     </div>
 
-    <div class="card mt-5">
-      <img src="../assets/Vehiculos/Kangoo.jpg" class="card-img-top"> 
-            <div class="card-body">     
-              <h5 class="card-title">KANGOO</h5>
-              <p class="card-text">$150 x día</p>
-             <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
-             <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button><br>
-            </div>
+    <div class="card mt-5 p-3">
+      <img src="../assets/Vehiculos/Kangoo.png" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">KANGOO</h5>
+        <p class="card-text">$150 x día</p>
+        <button @click="irDescripcion" class="btn btn-primary">Descripción</button>
+        <br />
+        <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button>
+        <br />
+      </div>
     </div>
-  
-    <div class="card mt-5">
-      <img src="../assets/Vehiculos/F100.jpg" class="card-img-top"> 
-            <div class="card-body">     
-              <h5 class="card-title">F100</h5>
-              <p class="card-text">$200 x día</p>
-              <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
-              <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button><br>
-            </div>
+
+    <div class="card mt-5 p-3">
+      <img src="../assets/Vehiculos/F100.png" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">F100</h5>
+        <p class="card-text">$200 x día</p>
+        <button @click="irDescripcion" class="btn btn-primary">Descripción</button>
+        <br />
+        <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button>
+        <br />
+      </div>
     </div>
-    
-    <div class="card mt-5">
-      <img src="../assets/Vehiculos/Camion.jpg" class="card-img-top"> 
-            <div class="card-body">     
-              <h5 class="card-title">CAMIÓN</h5>
-              <p class="card-text">$250 x día</p>
-              <button @click="irDescripcion" class="btn btn-primary">Descripción</button><br>
-              <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button><br>
-            </div>
+
+    <div class="card mt-5 p-3">
+      <img src="../assets/Vehiculos/Camion.png" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">CAMIÓN</h5>
+        <p class="card-text">$250 x día</p>
+        <button @click="irDescripcion" class="btn btn-primary">Descripción</button>
+        <br />
+        <button @click="irContratar" class="btn btn-primary mt-1">Contratar</button>
+        <br />
+      </div>
     </div>
-    
-          
   </div>
 </template>
 
-
-
-
-
-
 <script>
 export default {
-  name: 'Vehiculos',
-  methods:{
-    irDescripcion(){
-      this.$router.push('/ServAdicionales')
+  name: "Vehiculos",
+  methods: {
+    irDescripcion() {
+      this.$router.push("/ServAdicionales");
     },
-    irContratar(){
-      this.$router.push('/Formulario')
+    irContratar() {
+      this.$router.push("/Formulario");
     }
   }
-}
+};
 </script>

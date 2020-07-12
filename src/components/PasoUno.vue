998e7f444848bc061d9c6869033b58f3a52ecccc
silvanaesq <gonmaralv@gmail.com>
2020-07-12 02:41:51 -0300

    Agregado de iterator paso uno

--- src/components/PasoUno.vue
+++ src/components/PasoUno.vue
@@ -1,11 +1,48 @@
 <template>
   <div>
-    <h2>ARME SU PEDIDO</h2>
+    <h2 class="text-decoration-underline">ARME SU PEDIDO</h2>
     <v-row align="center">
       <v-col>
         <div class="mb-3 pa-3">
           <h4>1 - ¿Qué elementos desea trasladar?</h4>
-          <v-autocomplete
+          <v-container fluid>
+            <v-data-iterator :items="productos" :search="search" hide-default-footer>
+              <template v-slot:header>
+                <v-toolbar dark color="blue darken-3" class="mb-1">
+                  <v-text-field
+                    v-model="search"
+                    clearable
+                    flat
+                    solo-inverted
+                    hide-details
+                    label="Buscador de productos"
+                  ></v-text-field>
+                </v-toolbar>
+              </template>
+              <template v-slot:default>
+                <v-row>
+                  <v-col
+                    v-for="(producto, index) in productosFiltrados"
+                    :key="producto.nombre"
+                    cols="12"
+                    sm="6"
+                    md="4"
+                    lg="2"
+                  >
+                    <v-card>
+                      <v-card-title
+                        v-model="itemElegido"
+                        @click="submit(index)"
+                        class="justify-center font-weight-bold"
+                      >{{ producto.nombre }}</v-card-title>
+                      <v-divider></v-divider>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </template>
+            </v-data-iterator>
+          </v-container>
+          <!-- <v-autocomplete
             label="Seleccionar un elemento..."
             :items="productos"
             item-text="nombre"
@@ -24,7 +61,7 @@
 
           <v-btn block color="success" @click="submit">Agregar</v-btn>
         </div>
-        <div class="mb-3 pa-3">
+          <div class="mb-3 pa-3">-->
           <h4>2 - Servicios Adicionales</h4>
           <v-sheet class="py-4 px-8">
             <v-chip-group>
@@ -54,7 +91,7 @@
           <v-list color="grey lighten-3" v-if="itemsAgregados.length > 0">
             <v-list-item-subtitle class="text-sm-left">Productos:</v-list-item-subtitle>
             <v-list-item v-for="(item, index) in itemsAgregados" :key="item.nombre">
-              <v-list-item-content >
+              <v-list-item-content>
                 <v-list-item v-if="itemsAgregados.length > 0">
                   {{`${item.nombre}`}}
                   <v-chip color="red lighten-1" class="ma-2" label>{{item.cantidad}}</v-chip>
@@ -87,6 +124,8 @@
                 </v-btn>
               </v-list-item-action>
             </v-list-item>
+          </v-list>
+          <div v-if="itemsAgregados.length > 0 || serviciosAgregados.length > 0">
             <v-divider></v-divider>
             <v-chip
               class="mb-1"
@@ -95,7 +134,7 @@
               large
               label
             >Total = ${{ precioTotal }}</v-chip>
-          </v-list>
+          </div>
         </v-col>
       </v-sheet>
     </v-row>
@@ -112,11 +151,22 @@ export default {
       productos: [
         { nombre: "Caja", cantidad: 0, peso: 10 },
         { nombre: "Ropero", cantidad: 0, peso: 300 },
-        { nombre: "Cama 1 plaza", cantidad: 0, peso: 100 },
-        { nombre: "Cama 2 plazas", cantidad: 0, peso: 200 },
+        { nombre: "Cama King", cantidad: 0, peso: 100 },
+        { nombre: "Cama Queen", cantidad: 0, peso: 200 },
         { nombre: "Colchon", cantidad: 0, peso: 20 },
         { nombre: "Mesa de Luz", cantidad: 0, peso: 5 },
-        { nombre: "Mesa", cantidad: 0, peso: 50 }
+        { nombre: "Mesa", cantidad: 0, peso: 50 },
+        { nombre: "TV", cantidad: 0, peso: 10 },
+        { nombre: "Silla", cantidad: 0, peso: 5 },
+        { nombre: "Piano", cantidad: 0, peso: 400 },
+        { nombre: "Heladera", cantidad: 0, peso: 100 },
+        { nombre: "Lavarropas", cantidad: 0, peso: 100 },
+        { nombre: "Lampara", cantidad: 0, peso: 1 },
+        { nombre: "Monitor", cantidad: 0, peso: 5 },
+        { nombre: "Gabinete", cantidad: 0, peso: 10 },
+        { nombre: "Sillon", cantidad: 0, peso: 50 },
+        { nombre: "Comoda", cantidad: 0, peso: 60 },
+        { nombre: "Rack", cantidad: 0, peso: 40 }
       ],
       serviciosAdicionales: [],
       itemElegido: { nombre: "", cantidad: 0 },
@@ -124,7 +174,8 @@ export default {
       serviciosAgregados: [],
       servicioAdicional: {},
       vehiculos: [],
-      precioTotal: 0
+      precioTotal: 0,
+      search: ""
     };
   },
   methods: {
@@ -134,23 +185,23 @@ export default {
       "actualizarElementos",
       "actualizarServicios"
     ]),
-    submit() {
+    submit(indice) {
       let i = 0;
       while (
         i < this.itemsAgregados.length &&
-        this.itemElegido.nombre != this.itemsAgregados[i].nombre
+        this.productosFiltrados[indice].nombre != this.itemsAgregados[i].nombre
       ) {
         i++;
       }
       if (i < this.itemsAgregados.length) {
         this.itemsAgregados[i].cantidad++;
       } else {
-        this.itemElegido.cantidad++;
-        this.itemsAgregados.push(this.itemElegido);
+        this.productosFiltrados[indice].cantidad++;
+        this.itemsAgregados.push(this.productosFiltrados[indice]);
       }
-      this.$nextTick(() => {
-        this.itemElegido = null;
-      });
+      // this.$nextTick(() => {
+      //   this.itemElegido = null;
+      // });
       this.calcularPrecio();
     },
     sumarItem(item) {
@@ -219,6 +270,13 @@ export default {
         i++;
       }
       return this.vehiculos[i];
+    },
+    productosFiltrados() {
+      let resultado = [];
+      resultado = this.productos.filter(producto =>
+        producto.nombre.toLowerCase().includes(this.search.toLowerCase())
+      );
+      return resultado;
     }
   },
   created() {

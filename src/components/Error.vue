9eb8db3aeab2fbfb3383f2055723c66226f78517
Alvaro <gonmaralv@gmail.com>
2020-07-15 22:59:14 -0300

    agregado pagina de error

---
+++ src/components/Error.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="mx-auto mt-16">
+    <v-row align="center" justify="center">
+        <v-card class="pa-6">
+          <h1>No existe el recurso</h1>
+          <v-img class="center" width="200" src="https://cdn.onlinewebfonts.com/svg/img_283855.png"></v-img>
+        </v-card>
+    </v-row>
+  </v-container>
+</template>
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import ServAdicionales from '../views/ServAdicionales.vue'
 import MedioPago from '../views/MedioPago.vue'
 import FormularioStepper from '../components/FormularioStepper.vue'
 import Pedido from '../views/Pedido.vue'
+import Error from '../components/Error.vue';
 
 Vue.use(VueRouter)
 
@@ -34,6 +35,11 @@ const routes = [
     path: '/pedido',
     component: Pedido
   },
+  {
+    path: '*',
+    component: Error
+  },
+
 ]
 const router = new VueRouter({
   routes,
